9bfe325ea3c8b14aa53175ee709b3e59061da4b9
Jan Borchers <kame@jans-mbp.home>
2017-07-09 19:21:51 +0100

    Remove old debug prints

--- src/js/data/connection-service.js
+++ src/js/data/connection-service.js
@@ -2,13 +2,11 @@ export default class Connection
 {
   get url ()
   {
-    window.console.log('getting');
     return 'ws://interview.dev.ctx.ef.com/telemetry';
   }
 
   constructor ({onOpen, onClose, onMessage, onError})
   {
-    window.console.log("constructor:", this);
     this.connection = new WebSocket(this.url);
 
     this.connection.onopen = onOpen;

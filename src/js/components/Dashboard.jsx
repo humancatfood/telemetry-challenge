a1d5f2dfc55ffed51ff50d9f695aef507d1bf748
Jan Borchers <kame@jans-mbp.home>
2017-07-09 16:03:17 +0100

    Connect the dashboard on mounting

--- src/js/components/Dashboard.jsx
+++ src/js/components/Dashboard.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { connectDashboard, disconnectDashboard } from './../data/actions';
+
 
 
 @connect(state => ({
   speed: state.speed
-}))
+}), {
+  connectDashboard,
+  disconnectDashboard
+})
 export default class Dashboard extends React.Component
 {
+  componentDidMount()
+  {
+    this.props.connectDashboard();
+  }
+
+  componentWillUnmount()
+  {
+    this.props.disconnectDashboard();
+  }
+
   render ()
   {
     const { speed } = this.props;
